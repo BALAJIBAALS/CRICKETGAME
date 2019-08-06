0a7d28ea6de5873b757ccc71fb01835910efcfcb
BALAJIBAALS <thirubaalaaji@gmail.com>
2019-08-06 19:49:10 +0530

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,99 +1,53 @@
+package com.cricket;
 public class Player
 {
-	private String name;
-	private int runs_scored;
-	private int balls_faced;
-	private float strike_rate;
-	private int fours=0;
-	private int sixes=0;
-    private int wickets=0;
-	private int overs_bowled;
-	private int runs_given=0;
-	private float economy;
-	private Role role;
+    private String name;
+    private int playerid;
 
-	public static enum Role {
-		BATSMAN, BOWLER, ALLROUNDER;
-	}
+    private Role role;
+    DbConnection playerobj = new DbConnection();
 
-   public Player(String name, Role role) 
-   {
-		this.name = name;
-		this.role = role;
-	}
-
-   public void CountRuns(int r) 
-   {
-		runs_scored += r;
-   }
-
-	public void CountBallsfaced()
-	{
-		balls_faced++;
-	}
-
-	public void CountSix() 
-	{
-		sixes++;
-	}
-
-	public void CountFour() 
-	{
-		fours++;
-	}
-	public void CountWickets()
-	{
-		wickets++;
-	}
-
-	public void CountOvers()
-	{
-		overs_bowled++;
-	}
-    
-    public void RunsGiven(int r)
-    {
-    	runs_given+=r;
+    public static enum Role {
+        BATSMAN, BOWLER, ALLROUNDER;
     }
 
-    public String GetPlayerName()
-    {
-   	   return this.name;
-    }
- 
-   public float GetStrikerate()
-   {
-   	     	strike_rate = (runs_scored>0) ? ((runs_scored/balls_faced) * 100 ) : 0;   	         
-   	        return strike_rate;
-   }
-
-   public float GetEconomy()
-   {
-         economy = (overs_bowled >0) ? (runs_given/overs_bowled) : 0;
-         return economy;
-   }
-
-   public int GetSix()
-    {
-    	return sixes;
-    }
-   public int Getfour()
-    {
-    	return fours;
+    public Player(int playerid, String name, Role role) {
+        this.playerid = playerid;
+        this.name = name;
+        this.role = role;
     }
 
-   public int GetWickets()
-    {
-    	return wickets;
-    }
-    public int GetRunsScored()
-    {
-    	return runs_scored;
-    }
-    public int BallsFaced()
-    {
-    	return balls_faced;
+    public void CountRuns(int r) {
+        playerobj.runsScored(this.playerid, r);
     }
 
+    public void CountBallsfaced() {
+        playerobj.countBallsFaced(this.playerid);
+    }
+
+    public void CountSix() {
+        playerobj.countSix(this.playerid);
+    }
+
+    public void CountFour() {
+        playerobj.countFour(this.playerid);
+    }
+
+    public void CountWickets() {
+        playerobj.countWicket(this.playerid);
+    }
+
+    public void CountOvers() {
+        playerobj.countOversBowled(this.playerid);
+    }
+
+    public void RunsGiven(int r) {
+        playerobj.runsGiven(this.playerid, r);
+    }
+
+    public String GetPlayerName() {
+
+        return this.name;
+    }
 
 }
